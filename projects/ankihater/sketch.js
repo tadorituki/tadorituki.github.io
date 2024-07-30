9c87737bf5ef43d76bcb399c686c38b2745325d4
tadorituki <labridaethefish@gmail.com>
2024-07-31 04:19:07 +0900

    added card age display for testing

--- projects/ankihater/sketch.js
+++ projects/ankihater/sketch.js
@@ -44,6 +44,10 @@ var greenValue = 0
 var redValue = 0
 var velocity = 0
 var position = 0
+var currentAge = 5
+var displayAge = currentAge
+var yellowWidth = 4
+var dropVelocity = 0
 var s2 = function (sketch) {
 
 
@@ -89,9 +93,73 @@ onWrong = function() {
     p52.background(100, 100, 0)
     greenValue = 400;
     redValue = 400;
+    yellowWidth = 40;
+    dropVelocity = 0.1;
 }
 
 
 
 var p51 = new p5(s1, "canvas")
 var p52 = new p5(s2, "canvas2")
+
+var s3 = function (sketch) {
+
+
+    sketch.setup = function () {
+        let canvas3 = sketch.createCanvas(120, 450)
+        canvas3.parent('canvas_2')
+        sketch.background(255,255,255)
+        sketch.ellipseMode(sketch.RADIUS)
+    }
+
+    sketch.draw = function () {
+        displayAge = Math.max(displayAge-dropVelocity, nextAgeYellow[currentAge])
+        drawCircle(currentAge, displayAge)
+    }
+    
+}
+
+var p53 = new p5(s3, "canvas3")
+var nextAgeGreen = [2,2,3,5,6,6,7,7]
+var nextAgeYellow = [1,1,2,3,3,3,4,4]
+var nextAgeRed = [0,0,1,1,2,2,3,3]
+
+drawCircle = function(age, dage) {
+    p53.background(0)
+    p53.noStroke()
+    p53.fill(255, 255, 255, 188)
+    p53.beginShape()
+    p53.vertex(0, 450)
+    for(var x = 0; x < 120; x++){ 
+        var angle = (p53.frameCount*0.01)%(Math.PI*2) + x * 0.06
+        var y = p53.map(Math.sin(angle), -1, 1, 450-10-(dage*55+20), 450+10-(dage*55+20))
+        p53.vertex(x, y)
+    }
+    p53.vertex(120, 450)
+    p53.endShape()
+    
+    p53.fill(255, 255, 255, 66)
+    p53.beginShape()
+    p53.vertex(0, 450)
+    for(var x = 0; x < 120; x++){ 
+        var angle = (-p53.frameCount*0.01)%(Math.PI*2) + x * 0.06
+        var y = p53.map(Math.cos(angle), -1, 1, 450+10-(dage*55+20), 450-10-(dage*55+20))
+        p53.vertex(x, y)
+    }
+    p53.vertex(120, 450)
+
+    p53.endShape()
+
+    p53.strokeWeight(4);
+    p53.stroke(255, 0, 0, 60)
+    p53.line(0, 450-20-55*nextAgeRed[age], 120, 450-20-55*nextAgeRed[age])
+    
+    p53.strokeWeight(yellowWidth);
+    yellowWidth = Math.max(4, yellowWidth-0.5)
+    p53.stroke(255, 255, 0, 60)
+    p53.line(0, 450-20-55*nextAgeYellow[age], 120, 450-20-55*nextAgeYellow[age])
+    
+    p53.strokeWeight(4);
+    p53.stroke(0, 255, 0, 60)
+    p53.line(0, 450-20-55*nextAgeGreen[age], 120, 450-20-55*nextAgeGreen[age])
+}
